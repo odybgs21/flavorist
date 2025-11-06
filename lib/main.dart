732e28e22dea71e3cfb6e9173cab2b6fe37f7bd1
Rizky Wahyu <rizkywahyufadillah3@gmail.com>
2025-11-06 22:35:38 +0700

    penyesuaian tampilan register

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flavorist/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'theme.dart';
 import 'screens/login_screen.dart';
@@ -30,6 +31,7 @@ class _FlavoristState extends State<Flavorist> {
       debugShowCheckedModeBanner: false,
       theme: appTheme,
       home: const LoginScreen(),
+      // home: const HomeScreen(),
     );
   }
 }
